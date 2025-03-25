92d687d7abbc19badc5426048c029fb6b1f20a50
Anton Domnikov <>
2025-03-25 11:29:58 +0300

    add model for custom user

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -82,6 +83,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Internationalization
 
---
+++ api_yamdb/users/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField('Роль', max_length=64)
